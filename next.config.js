95f17ba9dfba0bf4e815f7add7fc92d42a10221e
reeeeemo <robbie@oxley.ca>
2024-11-15 11:57:15 -0500

    cleaned up next.config for server

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
     async rewrites() {
         return [
             {
                 source: '/api/:path*',
-                destination: 'http://127.0.0.1:5328/:path*' // backend proxy to the flask serv
-            },
+                destination: process.env.NODE_ENV === 'development'
+                    ? 'http://127.0.0.1:5328/:path*'  // Local development
+                    : '/api/:path*'  // Production/Vercel
+            }
         ]
-    },
+    }
 }
+
+module.exports = nextConfig
